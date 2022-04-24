ed68d57a888d4a69070a2df1bd3c58a8d5bc9214
pasongsong <stutk31@naver.com>
2022-04-24 23:03:44 +0900

    Update 738 Monotone Increasing Digits.java

--- 738 Monotone Increasing Digits.java
+++ 738 Monotone Increasing Digits.java
@@ -3,22 +3,17 @@
 class Solution {
     public int monotoneIncreasingDigits(int n) {
         char[] chars = Integer.toString(n).toCharArray();
-        for (int i = 0; i < chars.length - 1; ) {
-            if (chars[i + 1] >= chars[i]) {
+        for (int i = 0; i < chars.length - 1; ) {   // 0부터 끝까지 검사할거임
+            if (chars[i + 1] >= chars[i]) {  // 증가하면 한 칸 씩 뒤로 가고
                 i++;
             } else {
-                if (i > 0) {
-                    chars[i]--;
-                    for (int j = i + 1; j < chars.length; j++) {
-                        chars[j] = '9';
-                    }
-                    i--;
-                } else {
-                    chars[0]--;
-                    for (int j = 1; j < chars.length; j++) {
-                        chars[j] = '9';
-                    }
+                chars[i]--;  // 증가 안 하면 현재 자리에서 1빼고
+                for (int j = i + 1; j < chars.length; j++) {
+                    chars[j] = '9';   // 내 뒤는 9로 채움. 그게 제일 증가하면서 큰 숫자이니까
                 }
+                if (i > 0) {  // 현재 자리 숫자도 변경됐으니까 다시 검증해야 함.
+                    i--;      // 한 칸 앞으로 다시 돌아가서 검증하기
+                }             // 현재 자리가 맨 앞이면 불필요하니까 i > 0 일 때만
             }
         }
 
