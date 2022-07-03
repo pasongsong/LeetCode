3463c7eab28c605aaccd6a4e7fd3bf9614f735ae
pasongsong <stutk31@naver.com>
2022-07-03 22:20:27 +0900

    Create 665. Non-decreasing Array.java

---
+++ 665. Non-decreasing Array.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/non-decreasing-array
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int count = 0;                                                                
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] > nums[i]) {
+                count++;
+                if (i < 2 || nums[i - 2] <= nums[i]) {
+                    nums[i - 1] = nums[i];
+                } else {
+                    nums[i] = nums[i - 1];
+                }                                
+            }
+
+            if (count > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
